d922567a9eca0b7cfd92cc70f80f9ba5d21b4f07
Travis Cook <travis.cook5583@stu.mtec.edu>
2022-11-10 08:46:12 -0700

    Displaying only what cards wanted by type.

--- react-pokemon-app/src/Global/CardCreaters/createCardToRender.js
+++ react-pokemon-app/src/Global/CardCreaters/createCardToRender.js
@@ -4,13 +4,15 @@ import './cardstyles.css'
 
 export default function CreateCard(props){
     const pokemon_index = props.pokemonIndex ?? 1;
+    const typeToDisplay = props.typeToDisplay ?? ''
     const { name = '', picture_url = '', type1 = '', type2 = ''} = useGetPokiData(pokemon_index);
-    return(
+    const isDisplay = typeToDisplay === null || typeToDisplay === type1 || typeToDisplay === type2;
+    return isDisplay ? (
         <div className='card'>
             <h2 className='cardName'>{name}</h2>
             <img className='cardImage' src={picture_url}></img>
             <span className='cardType'>{type1}</span>
             <span className='cardType'>{type2}</span>
         </div>
-    )
+    ) : null
 }
--- react-pokemon-app/src/sections/TrC-Mt/Tsection.js
+++ react-pokemon-app/src/sections/TrC-Mt/Tsection.js
@@ -4,11 +4,15 @@ import CreateCard from '../../Global/CardCreaters/createCardToRender';
 
 
 export default function Tcode(){ //Remember to rename your section here
-
+    let wantedType = null;
+    
     return(
         <div>
             <div>T code here</div>
-            <CreateCard pokemonIndex={2} />
+            {new Array(100).fill(1).map((startVal, index) =>(
+                <CreateCard pokemonIndex={startVal + index} typeToDisplay={wantedType} />
+            ) )}
+           
         </div>
     )
 }
