b6be929912f1e82bb422d962ef1a4a5b8b7358a2
Travis Cook <travis.cook5583@stu.mtec.edu>
2022-11-10 09:56:45 -0700

    More cards.

--- react-pokemon-app/src/sections/TrC-Mt/Tsection.js
+++ react-pokemon-app/src/sections/TrC-Mt/Tsection.js
@@ -4,11 +4,15 @@ import CreateCard from '../../Global/CardCreaters/createCardToRender';
 
 
 export default function Tcode(){ //Remember to rename your section here
-
+    let wantedType = null;
+    
     return(
         <div>
             <div>T code here</div>
-            <CreateCard pokemonIndex={2} />
+            {new Array(180).fill(1).map((one, index) =>(
+                <CreateCard pokemonIndex={one + index} typeToDisplay={wantedType} key={index}/>
+            ) )}
+           
         </div>
     )
 }
