44a4d1e84710ca670f54e9f5a9af2f4dc75cf78d
Travis Cook <travis.cook5583@stu.mtec.edu>
2022-11-11 09:25:27 -0700

    Added component files to replace section files on the page. Figuring out how to get specific types rendering from a dropdown.

--- react-pokemon-app/src/sections/TrC-Mt/Tsection.js
+++ react-pokemon-app/src/sections/TrC-Mt/Tsection.js
@@ -1,18 +1,21 @@
 import React, {useEffect, useState} from 'react';
+import TypeOptions from '../../Global/Components/Tcomponents/TypeOptions'
 
 import CreateCard from '../../Global/CardCreaters/createCardToRender';
 
 
 export default function Tcode(){ //Remember to rename your section here
-    let wantedType = null;
-    
+    let [wantedType, setWantedType] = useState(null)
     return(
-        <div>
-            <div>T code here</div>
-            {new Array(180).fill(1).map((one, index) =>(
-                <CreateCard pokemonIndex={one + index} typeToDisplay={wantedType} key={index}/>
-            ) )}
-           
-        </div>
+        <>
+            <div>
+                <div style={{color: 'crimson'}}>T code here</div>
+                <TypeOptions onChange={() => console.log('Changed').then(setWantedType(this.value))}></TypeOptions>
+                {new Array(180).fill(1).map((one, index) =>(
+                    <CreateCard pokemonIndex={one + index} typeToDisplay={wantedType} key={index}/>
+                ) )}
+            
+            </div>
+        </>
     )
 }
