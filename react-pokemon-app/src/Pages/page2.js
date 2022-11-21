cff1abc10e31606feab642c3bc13a5ce5e303a15
Travis Cook <travis.cook5583@stu.mtec.edu>
2022-11-21 09:20:50 -0700

    Trying to fix things

--- react-pokemon-app/src/Pages/page2.js
+++ react-pokemon-app/src/Pages/page2.js
@@ -1,20 +1,11 @@
 import React from "react"
-<<<<<<< HEAD
+import Jcode from '../sections/hjake07/components/gameCode'
 
 export default function page2(){
 
     return (
         <>
-            <h1>Not yet built.</h1>
-            <h4>Page2</h4>
-=======
-import Jcode from '../sections/hjake07/components/gameCode'
-export default function Page2(){
-
-    return (
-        <>
             <Jcode />
->>>>>>> combine
         </>
     )
 }
