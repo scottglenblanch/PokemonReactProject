e75d415a91f746192ca63984414cda82f7aae7c9
Alex Paredes <alexparedes83@gmail.com>
2022-11-21 09:25:01 -0700

    changing things

--- react-pokemon-app/src/sections/Walls101/ThirdPage.js
+++ react-pokemon-app/src/sections/Walls101/ThirdPage.js
@@ -1,22 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import CreateCard from '../../Global/CardCreaters/createCardToRender';
-import '../hjake07/Jstyles.css'
 import '../Walls101/Wstyles.css'
+import '../hjake07/Jstyles.css'
+
 import DeckOption from './components/ChosenDeck';
 
 export default function DeckSelection(){
     return(
         <div className='pageThree'>
             <DeckOption></DeckOption>
-                <br></br>
-                <br></br>
-                <div className='front'>
-                    <CreateCard pokemonIndex={300} typeToDisplay={null}></CreateCard>
+            <br></br>
+            <br></br>
+            <div class='maincontainer'>
+                <div class='card' id='card'>
+                    <div class='front'>
+
+                    </div>
+                    <div class='back'>
+
+                    </div>
+
                 </div>
-                <div className='back'>
-                    <h4>Get Pranked!</h4>
-                </div>
+
             </div>
+        </div>
         
     )
 }
