2a31facdfdde9f4f6f4ad6743efd72d1bc896fb9
Jacob Davies <jacobdavies@Jacobs-MacBook-Pro.local>
2022-11-22 09:16:51 -0700

    done

--- react-pokemon-app/src/Global/CardCreaters/createCardToRender.js
+++ react-pokemon-app/src/Global/CardCreaters/createCardToRender.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {useGetPokiData} from './createCardDataObject';
 import './cardstyles.css'
-import { handleClick } from '../../sections/hjake07/Jsection';
 export default function CreateCard(props){
+    const onClick = props.onClick;
+    const isSelected = props.selected;
     const pokemon_index = props.pokemonIndex ?? 1;
     const typeToDisplay = props.typeToDisplay;
-    
     const { name = '', picture_url = '', type1 = '', type2 = ''} = useGetPokiData(pokemon_index);
     const isDisplay = typeToDisplay === null || typeToDisplay === type1 || typeToDisplay === type2;
     return isDisplay ? (
-        <div className='card'>
+        <div className={`card ${isSelected ? 'selected': ''}`} onClick={onClick}>
             <h2 className='cardName'>{name.charAt(0).toUpperCase() + name.slice(1)}</h2>
             <img className='cardImage' src={picture_url}></img>
             <div className="cardTypes">
--- react-pokemon-app/src/Global/Components/Jcomponents/gameCode.js
+++ react-pokemon-app/src/Global/Components/Jcomponents/gameCode.js
@@ -1,37 +1,109 @@
+import { useEffect, useState } from 'react';
+import HandleClick from '../../../sections/hjake07/Jsection';
 import CreateCard from '../../CardCreaters/createCardToRender'
 import Player1Score from './player1score'
 import Player2Score from './player2score';
 import Player3Score from './player3score';
 import Player4Score from './player4score';
 
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 export default function Jcode(){ //Remember to rename your section here
-    const defaultArray = Array.from({length: 81}, (_, i) => i + 1)
-    let randomArray = []
-        for(let j = 0; j < 20; j++){
-        randomArray.push(defaultArray[Math.floor(Math.random() * 81)])
+
+    const [displayArray, setDisplayArray] = useState([]);
+    const [firstCardSelectedPosition, setFirstCardSelectedPosition ] = useState(null);
+    const [secondCardSelectedPosition, setSecondCardSelectedPosition] = useState(null);
+
+    const resetSelectedPositions= () => {
+      setFirstCardSelectedPosition(null);
+      setSecondCardSelectedPosition(null);
     }
-    let arrayToSplit = [...new Set(randomArray)].slice(0, 12)
-    function shuffle(array) {
-        let currentIndex = array.length,  randomIndex;
-      
-        while (currentIndex != 0) {
-      
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex--;
-      
-          [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
-        }
-      
-        return array;
+
+    const updateWhenMatchFound = () => {
+      console.log('You found a match!')
+      displayArray[firstCardSelectedPosition].isHidden = true;
+      displayArray[secondCardSelectedPosition].isHidden = true;
+
+      setDisplayArray([...displayArray]);
+      resetSelectedPositions();
+
+    }
+
+    useEffect(() => {
+      const firstCardIndex = displayArray[firstCardSelectedPosition]?.index;
+      const secondCardIndex = displayArray[secondCardSelectedPosition]?.index;
+      const isBothSelected = firstCardSelectedPosition !== null && 
+      secondCardSelectedPosition !== null
+      const isMatch =  isBothSelected && 
+        firstCardIndex === secondCardIndex;
+      const isNotMatch = isBothSelected && !isMatch
+
+      if(isMatch) {
+       updateWhenMatchFound();
+      } else if(isNotMatch) {
+        console.log('Not a match!');
+        resetSelectedPositions();
       }
-    let shuffledArray = arrayToSplit.concat(arrayToSplit.slice())
+
+    }, [firstCardSelectedPosition, secondCardSelectedPosition, displayArray]);
+
+    useEffect(() => {
+      let displayArray;
+      const defaultArray = Array.from({length: 81}, (_, i) => i + 1)
+      let randomArray = []
+          for(let j = 0; j < 20; j++){
+            const randomNumber = Math.floor(Math.random() * 81)
+            randomArray.push(defaultArray[randomNumber])
+          }
+      const arrayWithUniqueTypes = [...new Set(randomArray)].slice(0, 12);
+      const displayArrayWithoutPosition = [
+        ...arrayWithUniqueTypes,
+        ...arrayWithUniqueTypes
+      ];
+
+      // shuffle
+      shuffleArray(displayArrayWithoutPosition);
+      
+      displayArray = displayArrayWithoutPosition.map((index, position) => ({
+        index, isHidden: false, position
+      }))
+
+      setDisplayArray(displayArray);
+    }, []);
+
+    const onClick = (pos) => {
+      const firstCardIndex = displayArray[firstCardSelectedPosition]?.index;
+      const secondCardIndex = displayArray[secondCardSelectedPosition]?.index;
+      const isSetFirstCard = isNaN(firstCardIndex);
+      const isSetSecondCard = !isSetFirstCard && isNaN(secondCardIndex);
+
+      if(isSetFirstCard) {
+        setFirstCardSelectedPosition(pos); 
+      } else if(isSetSecondCard) {
+        setSecondCardSelectedPosition(pos);
+      }
+    };
+    
     return(
             <div className='mySection' id="myTableSection">
                 <div className='container'>
-                      {shuffle(shuffledArray).map((index) =>(
-                        <div className="cards"><CreateCard pokemonIndex={1 + index} typeToDisplay={null} key={index}/></div> 
-                   ) )}
+                      {displayArray.map(({ index, isHidden, position }) => !isHidden ? (
+                        <div className="cards" >
+                          <CreateCard
+                            onClick={() => onClick(position)}
+                            key={position}
+                            pokemonIndex={1 + index} 
+                            typeToDisplay={null}
+                            selected={position === firstCardSelectedPosition || position === secondCardSelectedPosition} 
+                          />
+                        </div> 
+                   ) : null )}
                  
                         </div>
                 <Player1Score />
