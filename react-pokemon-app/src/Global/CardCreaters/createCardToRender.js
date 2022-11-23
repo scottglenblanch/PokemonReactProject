90304017fe653056da0d9c02a1c4de868c2b5aa4
Jacob Davies <jacobdavies@Jacobs-MacBook-Pro.local>
2022-11-22 18:37:52 -0700

    card back and front

--- react-pokemon-app/src/Global/CardCreaters/createCardToRender.js
+++ react-pokemon-app/src/Global/CardCreaters/createCardToRender.js
@@ -9,11 +9,11 @@ export default function CreateCard(props){
     const { name = '', picture_url = '', type1 = '', type2 = ''} = useGetPokiData(pokemon_index);
     const isDisplay = typeToDisplay === null || typeToDisplay === type1 || typeToDisplay === type2;
     return isDisplay ? (
-        <div className={`card ${isSelected ? 'selected': ''}`} onClick={onClick}>
-            <h2 className='cardName'>{name.charAt(0).toUpperCase() + name.slice(1)}</h2>
-            <img className='cardImage' src={picture_url}></img>
-            <div className="cardTypes">
-                <span className='cardType'>{type1} {type2}</span>
+        <div className={`backgroundTest ${isSelected ? 'selected card': ''}`} onClick={onClick}>
+            <h2 className={`cardNameHidden ${isSelected ? 'cardName': ''}`} >{name.charAt(0).toUpperCase() + name.slice(1)}</h2>
+            <img className={`cardImageHidden ${isSelected ? 'cardImage': ''}`} src={picture_url}></img>
+            <div className={`cardTypesHidden ${isSelected ? 'cardTypes': ''}`}>
+                <span className={`cardTypeHidden ${isSelected ? 'cardType': ''}`}>{type1} {type2}</span>
             </div>
         </div>
     ) : null //Can change to something else
